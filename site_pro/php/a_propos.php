7e96983b5fc3d867fa5a6d3f00e125355cd32664
Reuziault Gabriel <gabriel.reuziault@caensup.fr>
2023-12-21 09:34:03 +0100

    changement

--- site_pro/php/a_propos.php
+++ site_pro/php/a_propos.php
@@ -4,10 +4,11 @@
 //require_once("../YAML/yaml/yaml.php");
 $data = yaml_parse_file('YAML/a_propos.yaml');
 
+echo "<img id='img' src=".$data["photo"].">";
+
 echo "<h2>".$data["accroche"]."</h2>\n";
 
 echo "<p>".$data["paragraphe_presentation"]."</p>\n";
 
-echo "<img id='img' src=".$data["photo"].">";
 ?>
 </section>
