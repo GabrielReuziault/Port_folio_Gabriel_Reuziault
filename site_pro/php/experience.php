56965766f35a27a1a7b6383411a04837ab8bd778
Reuziault Gabriel <gabriel.reuziault@caensup.fr>
2023-12-21 08:31:54 +0100

    encore d'autre changement

--- site_pro/php/experience.php
+++ site_pro/php/experience.php
@@ -10,5 +10,5 @@ foreach($data AS $unStage){
 }
 ?>
 
-<p>Pour accéder à mon CV, cliquer <a href="cv.pdf">ici</a></p>
+<p>Pour accéder à mon CV, cliquer <a href="documents/cv.pdf">ici</a></p>
 </section>
