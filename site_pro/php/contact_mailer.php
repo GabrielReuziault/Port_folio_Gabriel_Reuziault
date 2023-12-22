387e689f7c3c44da1b1ce4a64333089b63e53f4a
Reuziault Gabriel <gabriel.reuziault@caensup.fr>
2023-12-22 10:27:02 +0100

    Update contact_mailer.php

--- site_pro/php/contact_mailer.php
+++ site_pro/php/contact_mailer.php
@@ -26,8 +26,7 @@ if(!empty($_POST)) {
     $mail = new PHPMailer(true);
  
     try {
-        //Server settings
-        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+        //Server settings                   //Enable verbose debug output
         $mail->isSMTP();                                            //Send using SMTP
         $mail->Host = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth = true;                                   //Enable SMTP authentication
@@ -60,12 +59,6 @@ if(!empty($_POST)) {
 
         $resp = $recaptcha->setExpectedHostname('srv1-vm-1143.sts-sio-caen.info')
                   ->verify($gRecaptchaResponse, $remoteIp);
-
-        if ($resp->isSuccess()) {
-            echo "Succes !";
-        } else {
-            var_dump($errors);
-        }
     }
 
     ?>
